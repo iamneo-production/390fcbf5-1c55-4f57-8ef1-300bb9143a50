179b015a245241b77762a84983b430115ba5ce92
Mittu22 <chetanachitikela22@gmail.com>
2023-05-10 07:53:48 +0000

    Commit by Admin

---
+++ cartest/src/main/java/cartest/App.java
@@ -0,0 +1,13 @@
+package cartest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
