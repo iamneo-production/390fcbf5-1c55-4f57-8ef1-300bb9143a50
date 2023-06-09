e2cbb95685d66292dbb7b3ea849ec39ca8f8cb8d
Bhavana-mugala <bhavanamugala@gmail.com>
2023-06-09 12:40:51 +0000

    webdriver setup

--- cucumberapp/src/test/java/cucumberapp/AppTest.java
+++ cucumberapp/src/test/java/cucumberapp/AppTest.java
@@ -27,38 +27,14 @@ public class AppTest
     {
         WebDriver driver;
         ChromeOptions chromeOptions = new ChromeOptions();
-        //System.out.println("webdriver.chrome.driver","/home/coder/project/workspace/cucumberapp/chromedriver");
+       
         driver=new RemoteWebDriver(new URL("http://localhost:4444"),chromeOptions);
-        //WebDriverManager.chromedriver().setup();
+       
         chromeOptions.addArguments("--disable-dev-shm-usage");
         chromeOptions.addArguments("--remote-allow-origins=*");
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--disable-dev-shm-usage");
-        //assertTrue( true );
-       // System.out.println("Hello");
        
-           //WebDriverManager.chromedriver().setup();
-     //  ChromeOptions options = new ChromeOptions();
-       // options.addArguments("--start-maximized");
-       // driver = new ChromeDriver(options);
-    
-        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        
-
-        
-
-
-
-       /*
-          		ChromeOptions options = new ChromeOptions();
-          		AbstractDriverOptions<?> abstractOptions = options;
-          		abstractOptions.addArguments("--start-maximized");
-          		abstractOptions.addArguments("--disable-notifications");
-          		abstractOptions.addArguments("--disable-infobars");
-          		abstractOptions.addArguments("--disable-popup-blocking");
-          		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-          		WebDriver driver = new ChromeDriver(options);
-              */
           	driver.quit();
         
     }
