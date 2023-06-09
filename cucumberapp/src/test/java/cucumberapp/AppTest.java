79378a9957d526c75aeb05afc38f20fd6a828a6a
Bhavana-mugala <bhavanamugala@gmail.com>
2023-06-09 12:24:06 +0000

    code

--- cucumberapp/src/test/java/cucumberapp/AppTest.java
+++ cucumberapp/src/test/java/cucumberapp/AppTest.java
@@ -1,6 +1,14 @@
 package cucumberapp;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.AbstractDriverOptions;
+import java.net.MalformedURLException;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.Assert.assertTrue;
+import java.net.URL;
 
 import org.junit.Test;
 
@@ -9,12 +17,50 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+  
+ //AbstractDriverOptions ChromeDriver=null;
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
+
+    public static void main(String args[]) throws MalformedURLException
     {
-        assertTrue( true );
+        WebDriver driver;
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //System.out.println("webdriver.chrome.driver","/home/coder/project/workspace/cucumberapp/chromedriver");
+        driver=new RemoteWebDriver(new URL("http://localhost:4444"),chromeOptions);
+        //WebDriverManager.chromedriver().setup();
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        //assertTrue( true );
+       // System.out.println("Hello");
+       
+           //WebDriverManager.chromedriver().setup();
+     //  ChromeOptions options = new ChromeOptions();
+       // options.addArguments("--start-maximized");
+       // driver = new ChromeDriver(options);
+    
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+
+        
+
+
+
+       /*
+          		ChromeOptions options = new ChromeOptions();
+          		AbstractDriverOptions<?> abstractOptions = options;
+          		abstractOptions.addArguments("--start-maximized");
+          		abstractOptions.addArguments("--disable-notifications");
+          		abstractOptions.addArguments("--disable-infobars");
+          		abstractOptions.addArguments("--disable-popup-blocking");
+          		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+          		WebDriver driver = new ChromeDriver(options);
+              */
+          	driver.quit();
+        
     }
+
 }
