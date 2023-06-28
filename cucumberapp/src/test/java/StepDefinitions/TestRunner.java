6753f3307ab16642e6ad20e30f31ea0da0ee26e2
nehakoutika <nehakoutika123@gmail.com>
2023-06-28 16:37:06 +0000

    commit 1

---
+++ cucumberapp/src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,16 @@
+package StepDefinitions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",glue={"StepDefinitions"},
+monochrome= false,
+plugin = {"pretty","html:target/HtmlReports/report.html"}
+
+)
+public class TestRunner {
+
+}
