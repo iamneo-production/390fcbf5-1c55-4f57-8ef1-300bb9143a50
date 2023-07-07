0e422ed883fbe8eb8372fc66afabb0a9bb2da0c6
nehakoutika <nehakoutika123@gmail.com>
2023-07-07 07:14:15 +0000

    updated

--- cucumberapp/src/test/java/StepDefinitions/stepdefinitions.java
+++ cucumberapp/src/test/java/StepDefinitions/stepdefinitions.java
@@ -3,7 +3,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
